2bced24108430d3796974ef329c9c001f68a1ed2
DevBarham <saheedibrahimdamilare@gmail.com>
2023-05-30 10:30:25 +0100

    go

--- main.tf
+++ main.tf
@@ -1,37 +1,3 @@
-variable "domains" {
-  type = list(object({
-    name        = string
-    ip_address  = string
-    record_type = string
-  }))
-  default = {"barham.com" = {ip_address = "1.44.33.66", record_type = "A" }
-            "AbuUmair.com" = {ip_address = "1.44.33.66", record_type = "A" }
-            "AbMoney.com" = {ip_address = "1.44.33.66", record_type = "CNAME" }
-  
-  
-  }
-}
-
-# resource "aws_route53_record" "domain_records" {
-#   count = length(var.domains)
-
-#   zone_id = aws_route53_zone.example_com.zone_id
-#   name    = var.domains[count.index].name
-#   type    = var.domains[count.index].record_type
-#   ttl     = 300  # Modify this TTL as per your requirement
-
-#   records = [var.domains[count.index].ip_address]
-# }
-variable "load_balancers" {
-  type    = map(string)
-  default = {
-    "Barham.com" = "load-balancer-1-alias.aws.com"
-    "AbuUmair.com" = "load-balancer-2-alias.aws.com"
-    "AbMoney.com" = "load-balancer-3-alias.aws.com"
-  }
-}
-
-
 module "instances" {
   source         = "./modules/instance"
   instance_types = ["t2.micro", "t2.medium", "t3.medium"]
@@ -77,27 +43,18 @@ resource "aws_security_group" "instance_sg" {
   }
 }
 
-
-
-
-
-resource "aws_route53_zone" "domains" {
-  for_each = toset(var.domains)
-
-  name = each.value
+variable "domain_records" {
+  type = map(object({
+    ip_address = string
+    record_type = string
+  }))
 }
 
-resource "aws_route53_record" "domain_records" {
-  for_each = aws_route53_zone.domains
+resource "aws_route53_record" "dns_records" {
+  for_each = var.domain_records
 
-  zone_id = aws_route53_zone.domains[each.key].zone_id
-  name    = "www.${each.key}"  # This prepends "www." to each domain name
-  type    = "A"
-  ttl     = 300  # This parameter determines the Time to Live (TTL) for the DNS record, which specifies how long the record should be cached
-
-  alias {
-    name                   = var.load_balancers[each.key]
-    zone_id                = "your-load-balancer-zone-id"  # Replace with the appropriate load balancer's zone ID
-    evaluate_target_health = true  # Set to true if you want health checks on the target
-  }
+  name    = each.key
+  type    = each.value.record_type
+  ttl     = 300
+  records = [each.value.ip_address]
 }
